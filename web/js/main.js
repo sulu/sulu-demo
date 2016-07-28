9a2eeb4f32da9de35149149aff83f158d28ed571
Rene Zumtobel <renezumtobel@gmail.com>
2016-07-28 15:12:49 +0200

    tabs integrated

--- web/js/main.js
+++ web/js/main.js
@@ -39,3 +39,17 @@ $(".js-services-carousel").slick({
     prevArrow: '<i class="ion-ios-arrow-back services-carousel-item-prev"></i>',
     nextArrow: '<i class="ion-ios-arrow-forward services-carousel-item-next"></i>'
 });
+
+jQuery(document).ready(function() {
+    jQuery('.about-history-tabs-links a').on('click', function(e)  {
+        var currentAttrValue = jQuery(this).attr('href');
+
+        // Show/Hide Tabs
+        jQuery('.about-history-tabs-content ' + currentAttrValue).show().siblings().hide();
+
+        // Change/remove current tab to active
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
+
+        e.preventDefault();
+    });
+});
