ae9b55e44c95e1d02cdd610f5debfd60545b1d30
Patrick Hopfner <patrick.hopfner@gmail.com>
2017-06-26 14:41:08 +0200

    Template clean up (#8)
    * Template clean up
    
    * Fixed StyleCI
    
    * Removed BlogController
    
    * Fixed default template

--- bin/console.php
+++ bin/console.php
@@ -11,8 +11,8 @@ use Symfony\Component\Debug\Debug;
 set_time_limit(0);
 
 $input = new ArgvInput();
-$env = $input->getParameterOption(array('--env', '-e'), getenv('SYMFONY_ENV') ?: 'dev');
-$debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(array('--no-debug', '')) && $env !== 'prod';
+$env = $input->getParameterOption(['--env', '-e'], getenv('SYMFONY_ENV') ?: 'dev');
+$debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(['--no-debug', '']) && $env !== 'prod';
 
 if ($debug) {
     Debug::enable();
