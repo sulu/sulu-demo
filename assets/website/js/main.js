da1fc6fb3b00aada9897c51b9c51e686a2553877
Luca Rath-Heel <luca@sulu.io>
2019-10-08 17:20:05 +0200

    add navbar component

--- assets/website/js/main.js
+++ assets/website/js/main.js
@@ -1,9 +1,12 @@
 import web from 'massive-web';
 import $ from 'jquery';
 import './polyfill';
+import NavigationToggler from './components/NavigationToggler';
 
 window.$ = window.jQuery = $;
 window.web = web;
+
+web.registerComponent('navigation-toggler', NavigationToggler);
 
 if (window.webComponents) {
     web.startComponents(window.webComponents);
