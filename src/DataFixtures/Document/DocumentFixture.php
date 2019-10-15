13dae603fb65078328e657fb0f7f559339716480
Luca Rath-Heel <luca@sulu.io>
2019-10-15 17:47:07 +0200

    add block types and fixtures

--- src/DataFixtures/Document/DocumentFixture.php
+++ src/DataFixtures/Document/DocumentFixture.php
@@ -79,6 +79,25 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'header_image' => [
                     'id' => $this->getMediaId('civil-literature.jpg'),
                 ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'Civil Literature',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>After releasing their record album in 2014, Marshall Plan spent more than one year bringing their passion for the rock music to the big concerthalls and arenas all over Great Britain – in 2015 worldwide. In this time the rockband started to grow together and wrote there second album.</p>',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>In 2010 Liam, the frontman of Civil Literature founded the band with his brother Garry, a well known guitar player and songwriter in Manchester. In 2011 Marc followed. His talent in playing the bass fullfilled the intense music vibes. Together they had one big dream: rocking the stage of the Royal Albert Hall. In 2016 they are as close as never before. With their new album, reaching records in Europe.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'I have always been a passionated songwriter. My greatest fullfillment is to touch people with my messages and to encourage them to live their dreams.',
+                        'quoteReference' => 'Liam Hendrickson',
+                    ],
+                ],
                 'structureType' => 'default',
             ],
             [
@@ -88,6 +107,25 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'subtitle' => 'Lorem ipsum dolor sit amet.',
                 'header_image' => [
                     'id' => $this->getMediaId('coyoos.jpg'),
+                ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'Coyoos',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>After releasing their record album in 2012, Coyoos spent more than one year bringing their passion for the folk music to the big concerthalls and arenas all over the United States – in 2015 worldwide. In this time the folkband started to grow together and wrote there second album.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'Travelling and singing is all I need. Discover new places and meeting inspiring people are the experiences you never forget. It is my source of creativity and inspiration.',
+                        'quoteReference' => 'Jack',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>In 2014 Jack started his music career in San Diego, California. His talent in playing the guitar made him famous in a short period of time. His one big dream: touring around the United States. In 2016 he is as close as never before – with his new album, reaching records in the US.</p>',
+                    ],
                 ],
                 'structureType' => 'default',
             ],
@@ -99,6 +137,25 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'header_image' => [
                     'id' => $this->getMediaId('marshall.jpg'),
                 ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'Marshall Plan',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>Releasing their record album in 2003, Marshall Plan spent more than one year bringing their passion for the rock music to the big concerthalls and arenas all over Great Britain – in 2015 worldwide. In this time the rockband started to grow together and wrote there second album.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'We love making music together and to inspire people around us with our songs. We come from a small town in the UK. It still feels unreal, to be known from Asia to the States.',
+                        'quoteReference' => 'Jason Mcconkey',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>In 2003 Alex, the frontman of Marshall Plan founded the band with his best friends Bronson, Albert and Ray. Those well known guitar player and songwriter in Liverpool. In 2007 Jason followed. His talent in playing the bass fullfilled the intense music vibes. Together they had one big dream: rocking the stage in front of the Times Square in New York. In 2016 they are as close as never before. With their new album, reaching records all over the world.</p>',
+                    ],
+                ],
                 'structureType' => 'default',
             ],
             [
@@ -108,6 +165,21 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'subtitle' => 'Lorem ipsum dolor sit amet.',
                 'header_image' => [
                     'id' => $this->getMediaId('dudelsack.jpg'),
+                ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'The Bagpipes',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>In the beginning they focused on traditional and contemporary music with innovative flair, before they concetrated on there classic bagpipe music. Short after releasing their record album in 1998, The Bagpipes spent more than one year bringing their passion for the folk music to the big concerthalls and arenas all over Scottland – in 2015 worldwide. In this time the folkband started to grow together and wrote there fourth album. Soon they become the Scottish folkband of the year 2003, 2005 and 2014.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'We started our career in the streets of Glasgow. There is nothing more real and authentic then playing street music. People like you, or they don\'t and pass you. You immediately get the reaction.',
+                        'quoteReference' => 'Steve Avril',
+                    ],
                 ],
                 'structureType' => 'default',
             ],
@@ -119,6 +191,21 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'header_image' => [
                     'id' => $this->getMediaId('tj-fury.jpg'),
                 ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'TJ Fury',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>In the beginning he focused on combinations between music and Hip Hop. Today he concentrated on stong powerful Lines in the scene of Hip Hop. After releasing his record album in 2011, TJ Fury spent more than one year bringing his passion for Hip Hop to the downtown clubs all around the big cities in the United States – in 2015 worldwide. In this time TJ Fury started to record their new album. Soon they got several awards.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'We love making music. Check out our new tracks.',
+                        'quoteReference' => 'TJ Fury',
+                    ],
+                ],
                 'structureType' => 'default',
             ],
             [
@@ -127,6 +214,12 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'subtitle' => 'We like to give you insights into what we do.',
                 'header_image' => [
                     'id' => $this->getMediaId('blog.jpg'),
+                ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'Coming soon!',
+                    ],
                 ],
                 'navigationContexts' => ['main'],
                 'structureType' => 'default',
@@ -137,6 +230,25 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
                 'subtitle' => 'We work hard, but we love what we do.',
                 'header_image' => [
                     'id' => $this->getMediaId('about.png'),
+                ],
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'International Talents',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<h3>International Talents was founded 1998</h3><p>From Great Britain all over the world International Talents become one of the worldwide leading music brand. With over 20 years of recorded music history, our passion for artistry in music continues today. We love to inspire young talents with all of our knowledge and experience.&nbsp;The desire to speak into the heart and soul of the listeners is what fueled the creative and strategic efforts of the label.</p>',
+                    ],
+                    [
+                        'type' => 'quote',
+                        'quote' => 'The whole experience of 20 years and a lot of knowledge come together in International Talents. We love what we do, and no day is like the one before.',
+                        'quoteReference' => 'Jonathan Benett',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<p>But all this, is not possible, without a Team behind. This Team is attendable around the clock. They prepare your Event, help for your exhibition or Product Presentation. Everyone of them is an urban legend in what they do. Success is no accident. It grows with an great Team.</p>',
+                    ],
                 ],
                 'navigationContexts' => ['main', 'footer'],
                 'structureType' => 'default',
@@ -160,14 +272,32 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
         /** @var HomeDocument $homeDocument */
         $homeDocument = $documentManager->find('/cmf/demo/contents', AppFixtures::LOCALE);
 
-        /** @var BasePageDocument $teaserDocument */
-        $teaserDocument = $documentManager->find('/cmf/demo/contents/artists/the-bagpipes', AppFixtures::LOCALE);
+        /** @var BasePageDocument $aboutDocument */
+        $aboutDocument = $documentManager->find('/cmf/demo/contents/about-us', AppFixtures::LOCALE);
+
+        /** @var BasePageDocument $headerTeaserDocument */
+        $headerTeaserDocument = $documentManager->find('/cmf/demo/contents/artists/the-bagpipes', AppFixtures::LOCALE);
 
         $homeDocument->getStructure()->bind(
             [
                 'title' => $homeDocument->getTitle(),
                 'url' => '/',
-                'teaser' => $teaserDocument->getUuid(),
+                'teaser' => $headerTeaserDocument->getUuid(),
+                'blocks' => [
+                    [
+                        'type' => 'heading',
+                        'heading' => 'Our Label',
+                    ],
+                    [
+                        'type' => 'text',
+                        'text' => '<h3>International Talents was founded 1998</h3><p>From Great Britain all over the world International Talents become one of the worldwide leading music brand. With over 20 years of recorded music history, our passion for artistry in music continues today. We love to inspire young talents with all of our knowledge and experience.&nbsp;The desire to speak into the heart and soul of the listeners is what fueled the creative and strategic efforts of the label.</p>',
+                    ],
+                    [
+                        'type' => 'link',
+                        'page' => $aboutDocument->getUuid(),
+                        'text' => 'READ MORE',
+                    ],
+                ],
             ]
         );
 
@@ -187,7 +317,7 @@ class DocumentFixture implements DocumentFixtureInterface, ContainerAwareInterfa
 
         $data['elements'] = [
             'sortBy' => 'published',
-            'sortMethod' => 'desc',
+            'sortMethod' => 'asc',
             'dataSource' => $artistsDocument->getUuid(),
         ];
 
