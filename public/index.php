9fc583231f9dd6f043d7f8d44d6c6946ab00c04c
Alexander Schranz <alexander@sulu.io>
2020-07-16 17:03:16 +0200

    Change admin route

--- public/index.php
+++ public/index.php
@@ -46,7 +46,7 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
 
 $suluContext = SuluKernel::CONTEXT_WEBSITE;
 
-if (preg_match('/^\/admin(\/|$)/', $_SERVER['REQUEST_URI'])) {
+if (preg_match('/^\/my-custom-admin(\/|$)/', $_SERVER['REQUEST_URI'])) {
     $suluContext = SuluKernel::CONTEXT_ADMIN;
 }
 
