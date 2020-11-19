9b240f920fb72f4bf64198d570bced3a41bbe357
Niklas Natter <niklas@sulu.io>
2020-11-19 11:50:51 +0100

    Set jackalope.jackrabbit_version parameter

--- config/packages/phpcr.php
+++ config/packages/phpcr.php
@@ -1,9 +1,23 @@
 <?php
 
 if ('doctrinedbal' === $_ENV['PHPCR_BACKEND']) {
-    $container->setParameter('app.phpcr_backend', ['type' => 'doctrinedbal']);
+    $container->setParameter(
+        'app.phpcr_backend',
+        [
+            'type' => 'doctrinedbal',
+        ]
+    );
 } elseif ('jackrabbit' === $_ENV['PHPCR_BACKEND']) {
-    $container->setParameter('app.phpcr_backend', ['type' => 'jackrabbit', 'url' => '%env(PHPCR_BACKEND_URL)%']);
+    $container->setParameter(
+        'app.phpcr_backend',
+        [
+            'type' => 'jackrabbit',
+            'url' => '%env(PHPCR_BACKEND_URL)%',
+            'parameters' => [
+                'jackalope.jackrabbit_version' => '%env(JACKRABBIT_VERSION)%',
+            ],
+        ]
+    );
 
     $container->loadFromExtension(
         'sulu_document_manager',
