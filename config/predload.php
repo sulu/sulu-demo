0ef46a3535b235fae3079a020f629550b6a73002
Niklas Natter <niklas@sulu.io>
2021-05-27 14:44:36 +0200

    Remove config/predload.php file

--- config/predload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// if (file_exists(dirname(__DIR__).'/var/cache/admin/prod/App_KernelProdContainer.preload.php')) {
-//    require dirname(__DIR__).'/var/cache/admin/prod/App_KernelProdContainer.preload.php';
-// }
-
-// if (file_exists(dirname(__DIR__).'/var/cache/website/prod/App_KernelProdContainer.preload.php')) {
-//     require dirname(__DIR__).'/var/cache/website/prod/App_KernelProdContainer.preload.php';
-// }
